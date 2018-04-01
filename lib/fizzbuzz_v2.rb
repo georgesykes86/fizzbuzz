0f647f71f6a83cf6d63e65d4be6e5cb31c72b436
George Sykes <georgesykes86@gmail.com>
2018-04-01 12:30:19 +0100

    Added tests to fizzbuzz_v2

--- lib/fizzbuzz_v2.rb
+++ lib/fizzbuzz_v2.rb
@@ -2,15 +2,13 @@ class Integer
 
   def fizzbuzz
     if self % 3 == 0 && self % 5 == 0
-      puts 'fizzbuzz'
+      'fizzbuzz'
     elsif self % 5 == 0
-      puts 'buzz'
+      'buzz'
     elsif self % 3 == 0
-      puts 'fizz'
+      'fizz'
     else
-      puts self.to_s
+      self.to_s
     end
   end
 end
-
-9.fizzbuzz
---
+++ spec/fizzbuzz_v2_spec.rb
@@ -0,0 +1,7 @@
+require 'fizzbuzz_v2'
+
+describe 'fizzbuzz_v2' do
+  it 'returns "fizz" when passed 3' do
+    expect(3.fizzbuzz).to eq 'fizz'
+  end
+end
