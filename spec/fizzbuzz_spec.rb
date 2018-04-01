aaf25408e7708d96683af1a2e60cac6bf840e9ac
George Sykes <georgesykes86@gmail.com>
2018-04-01 12:13:35 +0100

    Solution code and tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -16,4 +16,19 @@ describe 'fizzbuzz' do
   it 'return "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
+
+  it 'returns "fizzbuzz" when passed a number that is a multiple of both 3 and 5' do
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
+  end
+
+  it 'returns "fizz" when passed a number that is a multiple of 3' do
+    expect(fizzbuzz(9)).to  eq "fizz"
+  end
+  numbers = [5,10,20,25]
+  numbers.each do |num|
+    it 'returns "buzz" when passed a number that is a multiple of 5' do
+      expect(fizzbuzz(num)).to eq "buzz"
+    end
+  end
+
 end
