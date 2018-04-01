3fb2d0abd987d578a06a8395e53f8f11d83034f1
George Sykes <georgesykes86@gmail.com>
2018-04-01 12:33:46 +0100

    Added more tests to fizzbuzz_v2

--- spec/fizzbuzz_v2_spec.rb
+++ spec/fizzbuzz_v2_spec.rb
@@ -4,4 +4,17 @@ describe 'fizzbuzz_v2' do
   it 'returns "fizz" when passed 3' do
     expect(3.fizzbuzz).to eq 'fizz'
   end
+
+  it 'returns "buzz" when passed 5' do
+    expect(5.fizzbuzz).to eq 'buzz'
+  end
+
+  it 'returns a number when that number is not divisible by 3 or 5' do
+    expect(4.fizzbuzz).to eq '4'
+  end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
+  end
+
 end
